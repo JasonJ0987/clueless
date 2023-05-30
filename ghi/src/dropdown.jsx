ea54ec1afdebf18a33a93257ae29d28645279c63
sabrina <sabrinablinder@gmail.com>
2023-05-30 12:43:16 -0400

    starter navbar react and css

---
+++ ghi/src/dropdown.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './dropdown.css';
+
+function Dropdown() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
+  return (
+    <ul
+      onClick={handleClick}
+      className={click ? 'dropdown-menu clicked' : 'dropdown-menu'}
+    >
+      <li>
+        <Link className="dropdown-link" to="/closet/{closet_id}/bins/">
+          Bins
+        </Link>
+      </li>
+      <li>
+        <Link className="dropdown-link" to="/closet/{closet_id}/bin/{bin_id/hats/">
+          Hats
+        </Link>
+      </li>
+      <li>
+        <Link className="dropdown-link" to="/closet/{closet_id}/bin/{bin_id/tops">
+          Tops
+        </Link>
+      </li>
+      <li>
+        <Link className="dropdown-link" to="/closet/{closet_id}/bin/{bin_id/bottoms">
+          Bottoms
+        </Link>
+      </li>
+      <li>
+        <Link className="dropdown-link" to="/closet/{closet_id}/bin/{bin_id/shoes">
+          Shoes
+        </Link>
+      </li>
+    </ul>
+  );
+}
+
+export default Dropdown;
