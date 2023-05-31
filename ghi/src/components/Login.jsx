011adb909a8a019dcba7c7b636f64d16d51739c1
Jeanette <yanette.cruzz@icloud.com>
2023-05-31 12:28:24 -0400

    journal

--- ghi/src/components/Login.jsx
+++ ghi/src/components/Login.jsx
@@ -2,36 +2,27 @@ import useToken from "@galvanize-inc/jwtdown-for-react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+
 const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [errorMessage, setErrorMessage] = useState("");
-    const { login, token } = useToken();
+    const { login } = useToken();
     const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
-        // console.log(`email: ${email} password: ${password}`);
+        console.log(`email: ${email} password: ${password}`);
         login(email, password);
-        // console.log("token:", token)
-        // works if you hit login button twice, need to readdress
-        if (token) {
-            setErrorMessage("");
-            e.target.reset();
-            console.log(token);
-            navigate("/");
-        } else {
-            e.target.reset();
-            setErrorMessage("Username/password was entered incorrectly");
-        }
+        e.target.reset();
+        navigate("/");
     };
 
-return (
+    return (
     <div className="card text-bg-light mb-3">
-        <h5 className="card-header">Login</h5>
-        <div className="card-body">
-        <form onSubmit={handleSubmit}>
-            <div className="mb-3">
+    <h5 className="card-header">Login</h5>
+    <div className="card-body">
+        <form onSubmit={(e) => handleSubmit(e)}>
+        <div className="mb-3">
             <label className="form-label">Email:</label>
             <input
                 name="email"
@@ -39,8 +30,8 @@ return (
                 className="form-control"
                 onChange={(e) => setEmail(e.target.value)}
             />
-        </div>
-        <div className="mb-3">
+            </div>
+            <div className="mb-3">
             <label className="form-label">Password:</label>
             <input
                 name="password"
@@ -48,14 +39,9 @@ return (
                 className="form-control"
                 onChange={(e) => setPassword(e.target.value)}
             />
-        </div>
-        {errorMessage && (
-            <div className="alert alert-danger">
-                {errorMessage}
             </div>
-        )}
             <div>
-                <input className="btn btn-primary" type="submit" value="Login" />
+            <input className="btn btn-primary" type="submit" value="Login" />
             </div>
         </form>
         </div>
