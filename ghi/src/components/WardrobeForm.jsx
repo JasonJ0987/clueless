bd653050d6b566136a2409cd5e33f09f69c67eda
Jeanette <yanette.cruzz@icloud.com>
2023-06-01 16:08:04 -0400

    fixing errors

--- ghi/src/components/WardrobeForm.jsx
+++ ghi/src/components/WardrobeForm.jsx
@@ -18,93 +18,93 @@ const WardrobeForm = () => {
   const [shoes, setShoes] = useState(null);
   const { token } = useToken();
 
-    const loadBins = async () => {
-      const url = `${process.env.REACT_APP_WHATEVR}/api/closet/646b99c3f2cd73044cf5707d/bins`;
-      const fetchConfig = {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      };
-      const response = await fetch(url, fetchConfig);
-      if (response.ok) {
-        const data = await response.json();
-        setBin(data);
-        console.log(bins);
-      }
+  const loadBins = async () => {
+    const url = `${process.env.REACT_APP_WHATEVR}/api/closet/646b99c3f2cd73044cf5707d/bins`;
+    const fetchConfig = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
     };
+    const response = await fetch(url, fetchConfig);
+    if (response.ok) {
+      const data = await response.json();
+      setBin(data);
+      console.log(bin);
+    }
+  };
 
-    // const loadHats = async () => {
-    //     const url = `${process.env.REACT_APP_WHATEVR}/api/closet/${closetId}/bins/646bc0f74277954dd0f38117/clothes`;
-    //     const fetchConfig = {
-    //         method: "GET",
-    //         headers: {
-    //             "Content-Type": "application/json",
-    //             Authorization: `Bearer ${token}`,
-    //         },
-    //     };
-    //     const response = await fetch(url, fetchConfig);
-    //     if (response.ok) {
-    //         const data = await response.json();
-    //         console.log("hatdata", data)
-    //         setHats(data)
-    //         //console.log(hats);
-    //     }
-    // };
+  // const loadHats = async () => {
+  //     const url = `${process.env.REACT_APP_WHATEVR}/api/closet/${closetId}/bins/646bc0f74277954dd0f38117/clothes`;
+  //     const fetchConfig = {
+  //         method: "GET",
+  //         headers: {
+  //             "Content-Type": "application/json",
+  //             Authorization: `Bearer ${token}`,
+  //         },
+  //     };
+  //     const response = await fetch(url, fetchConfig);
+  //     if (response.ok) {
+  //         const data = await response.json();
+  //         console.log("hatdata", data)
+  //         setHats(data)
+  //         //console.log(hats);
+  //     }
+  // };
 
-      // const loadTops = async () => {
-      //   const url = `${process.env.REACT_APP_WHATEVR}/api/closet/${closetId}/bins/646beb5724b33168d5719493/clothes`;
-      //   const fetchConfig = {
-      //     method: "GET",
-      //     headers: {
-      //       "Content-Type": "application/json",
-      //       Authorization: `Bearer ${token}`,
-      //     },
-      //   };
-      //   const response = await fetch(url, fetchConfig);
-      //   if (response.ok) {
-      //     const data = await response.json();
-      //     console.log("topdata", data)
-      //     setTops(data);
-      //     // console.log(tops);
-      //   }
-      // };
+  // const loadTops = async () => {
+  //   const url = `${process.env.REACT_APP_WHATEVR}/api/closet/${closetId}/bins/646beb5724b33168d5719493/clothes`;
+  //   const fetchConfig = {
+  //     method: "GET",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       Authorization: `Bearer ${token}`,
+  //     },
+  //   };
+  //   const response = await fetch(url, fetchConfig);
+  //   if (response.ok) {
+  //     const data = await response.json();
+  //     console.log("topdata", data)
+  //     setTops(data);
+  //     // console.log(tops);
+  //   }
+  // };
 
-      // const loadBottoms = async () => {
-      //   const url = `${process.env.REACT_APP_WHATEVR}/api/closet/${closetId}/bins/647659f829d0764ee8697289/clothes`;
-      //   const fetchConfig = {
-      //     method: "GET",
-      //     headers: {
-      //       "Content-Type": "application/json",
-      //       Authorization: `Bearer ${token}`,
-      //     },
-      //   };
-      //   const response = await fetch(url, fetchConfig);
-      //   if (response.ok) {
-      //     const data = await response.json();
-      //     console.log("bottomdata", data)
-      //     setBottoms(data);
-      //   }
-      // };
+  // const loadBottoms = async () => {
+  //   const url = `${process.env.REACT_APP_WHATEVR}/api/closet/${closetId}/bins/647659f829d0764ee8697289/clothes`;
+  //   const fetchConfig = {
+  //     method: "GET",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       Authorization: `Bearer ${token}`,
+  //     },
+  //   };
+  //   const response = await fetch(url, fetchConfig);
+  //   if (response.ok) {
+  //     const data = await response.json();
+  //     console.log("bottomdata", data)
+  //     setBottoms(data);
+  //   }
+  // };
 
-      // const loadShoes = async () => {
-      //   const url = `${process.env.REACT_APP_WHATEVR}/api/closet/${closetId}/bins/64765a3929d0764ee869728a/clothes`;
-      //   const fetchConfig = {
-      //     method: "GET",
-      //     headers: {
-      //       "Content-Type": "application/json",
-      //       Authorization: `Bearer ${token}`,
-      //     },
-      //   };
-      //   const response = await fetch(url, fetchConfig);
-      //   if (response.ok) {
-      //     const data = await response.json();
-      //     console.log("shoedata", data)
-      //     setShoes(data);
-      //     console.log(shoes);
-      //   }
-      // };
+  // const loadShoes = async () => {
+  //   const url = `${process.env.REACT_APP_WHATEVR}/api/closet/${closetId}/bins/64765a3929d0764ee869728a/clothes`;
+  //   const fetchConfig = {
+  //     method: "GET",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       Authorization: `Bearer ${token}`,
+  //     },
+  //   };
+  //   const response = await fetch(url, fetchConfig);
+  //   if (response.ok) {
+  //     const data = await response.json();
+  //     console.log("shoedata", data)
+  //     setShoes(data);
+  //     console.log(shoes);
+  //   }
+  // };
 
   useEffect(() => {
     // loadHats();
@@ -116,50 +116,50 @@ const WardrobeForm = () => {
 
   return (
     <p>jh</p>
-//     <div
-//       style={{ display: "flex", flexDirection: "column", alignItems: "center", height: "100vh" }}
-//     >
-//       <form onSubmit={handleSubmit}></form>
-//       <h3>Hats</h3>
-//       <div style={boxStyle}>
-//         {hats.map((hat) => (
-//           <div key={hat.id}>
-//             <img src={bin.picture}/>
-//           </div>
-//         ))}
-//       </div>
-//       <h3>Tops</h3>
-//       <div style={boxStyle}>
-//         {tops.map((top) => (
-//           <div key={top.id}>
-//             <img src={bin.picture} />
-//           </div>
-//         ))}
-//       </div>
-//       <h3>Bottoms</h3>
-//       <div style={boxStyle}>
-//         {bottoms.map((bottom) => (
-//           <div key={bottom.id}>
-//             <img src={bin.picture} />
-//           </div>
-//         ))}
-//       </div>
-//       <h3>Shoes</h3>
-//       <div style={boxStyle}>
-//         {shoes.map((shoe) => (
-//           <div key={shoe.id}>
-//             <img src={bin.picture} />
-//           </div>
-//         ))}
-//       </div>
-//       <div>
-//         <input className="btn btn-primary" type="submit" value="Submit Style!" />
-//       </div>
-//     </div>
-//   );
-// };
-  )
-}
+    //     <div
+    //       style={{ display: "flex", flexDirection: "column", alignItems: "center", height: "100vh" }}
+    //     >
+    //       <form onSubmit={handleSubmit}></form>
+    //       <h3>Hats</h3>
+    //       <div style={boxStyle}>
+    //         {hats.map((hat) => (
+    //           <div key={hat.id}>
+    //             <img src={bin.picture}/>
+    //           </div>
+    //         ))}
+    //       </div>
+    //       <h3>Tops</h3>
+    //       <div style={boxStyle}>
+    //         {tops.map((top) => (
+    //           <div key={top.id}>
+    //             <img src={bin.picture} />
+    //           </div>
+    //         ))}
+    //       </div>
+    //       <h3>Bottoms</h3>
+    //       <div style={boxStyle}>
+    //         {bottoms.map((bottom) => (
+    //           <div key={bottom.id}>
+    //             <img src={bin.picture} />
+    //           </div>
+    //         ))}
+    //       </div>
+    //       <h3>Shoes</h3>
+    //       <div style={boxStyle}>
+    //         {shoes.map((shoe) => (
+    //           <div key={shoe.id}>
+    //             <img src={bin.picture} />
+    //           </div>
+    //         ))}
+    //       </div>
+    //       <div>
+    //         <input className="btn btn-primary" type="submit" value="Submit Style!" />
+    //       </div>
+    //     </div>
+    //   );
+    // };
+  );
+};
 
 const boxStyle = {
   width: "200px",
