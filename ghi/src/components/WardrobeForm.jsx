53fe2603b77bd76c7c5be185c0085479902d2f65
Jeanette <yanette.cruzz@icloud.com>
2023-06-04 20:02:48 -0400

    fixed form

--- ghi/src/components/WardrobeForm.jsx
+++ ghi/src/components/WardrobeForm.jsx
@@ -3,87 +3,33 @@ import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 
 const WardrobeForm = () => {
-  const [bins, setBin] = useState([]);
   const [hats, setHats] = useState([]);
-  const [tops, setTops] = useState(null);
-  const [bottoms, setBottoms] = useState(null);
-  const [shoes, setShoes] = useState(null);
+  const [hat, setHat] = useState(null);
+  const [tops, setTops] = useState([]);
+  const [top, setTop] = useState(null);
+  const [bottoms, setBottoms] = useState([]);
+  const [bottom, setBottom] = useState(null);
+  const [shoes, setShoes] = useState([]);
+  const [shoe, setShoe] = useState(null);
   const { token } = useToken();
 
-  const handleHatsChange = (event) => {
-    const value = event.target.value;
-    setHats(value);
-  };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const data = {
-      hats,
-      tops,
-      bottoms,
-      shoes
-    };
-
-    const response = await fetch(
-      `${process.env.REACT_APP_WHATEVR}/api/wardrobe`,
-      {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
-    if (response.ok) {
-      setHats([]);
-      setTops([]);
-      setBottoms([]);
-      setShoes([]);
-    } else {
-      const error = await response.json();
-      console.log("Error", error)
-    }
-  };
-
-//   const loadBins = async () => {
-//     const url = `${process.env.REACT_APP_WHATEVR}/api/closet/646b99c3f2cd73044cf5707d/bins/`;
-//     const fetchConfig = {
-//       method: "GET",
-//       headers: {
-//         "Content-Type": "application/json",
-//         // Authorization: `Bearer ${token}`,
-//       },
-//     };
-//     const response = await fetch(url, fetchConfig);
-//     if (response.ok) {
-//       const data = await response.json();
-//       console.log("data", data)
-//       setBin(data.bins);
-//     }
-//   };
-
-  const loadHats = async () => {
+    const loadHats = async () => {
     const url = `${process.env.REACT_APP_WHATEVR}/api/closet/646b99c3f2cd73044cf5707d/bins/646bc0f74277954dd0f38117/clothes`;
     const fetchConfig = {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        // Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${token}`,
       },
     };
     const response = await fetch(url, fetchConfig);
     if (response.ok) {
       const data = await response.json();
-      let hatList = [];
-      for (let hat of data.clothes) {
-        hatList.push(hat);
-      }
-      console.log("hatdata", data);
-      setHats(hatList);
-      console.log("hats", hats);
+      setHats(data.clothes)
     }
   };
+
 
   const loadTops = async () => {
     const url = `${process.env.REACT_APP_WHATEVR}/api/closet/646b99c3f2cd73044cf5707d/bins/646beb5724b33168d5719493/clothes`;
@@ -91,17 +37,16 @@ const WardrobeForm = () => {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        // Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${token}`,
       },
     };
     const response = await fetch(url, fetchConfig);
     if (response.ok) {
       const data = await response.json();
-      // console.log("topdata", data)
-      setTops(data.clothes);
-      console.log("tops", tops);
+      setTops(data.clothes)
     }
   };
+
 
   const loadBottoms = async () => {
     const url = `${process.env.REACT_APP_WHATEVR}/api/closet/646b99c3f2cd73044cf5707d/bins/647659f829d0764ee8697289/clothes`;
@@ -109,17 +54,16 @@ const WardrobeForm = () => {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        // Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${token}`,
       },
     };
     const response = await fetch(url, fetchConfig);
     if (response.ok) {
       const data = await response.json();
-      // console.log("bottomdata", data)
       setBottoms(data.clothes);
-      console.log("bottoms", bottoms);
     }
   };
+
 
   const loadShoes = async () => {
     const url = `${process.env.REACT_APP_WHATEVR}/api/closet/646b99c3f2cd73044cf5707d/bins/64765a3929d0764ee869728a/clothes`;
@@ -127,158 +71,156 @@ const WardrobeForm = () => {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        // Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${token}`,
       },
     };
     const response = await fetch(url, fetchConfig);
     if (response.ok) {
       const data = await response.json();
-      // console.log("shoesdata", data)
       setShoes(data.clothes);
-      console.log("shoes", shoes);
     }
   };
+
 
   useEffect(() => {
     loadHats();
     loadTops();
     loadBottoms();
     loadShoes();
-    // loadBins();
   }, [token]);
 
-  // useEffect(() => {
-  //     loadHats();
-  // }, []);
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = {
+      hat,
+      top,
+      bottom,
+      shoe,
+    };
 
-  // useEffect(() => {
-  //     loadTops();
-  // }, []);
+    try {
+      const response = await fetch(`${process.env.REACT_APP_WHATEVR}/api/wardrobe`, {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
 
-  // useEffect(() => {
-  //     loadBottoms();
-  // }, []);
+      if (response.ok) {
+        setHats([]);
+        setTops([]);
+        setBottoms([]);
+        setShoes([]);
+      } else {
+        const error = await response.json();
+        console.log("Error", error);
+      }
+    } catch (error) {
+      console.log("Error", error);
+    }
+  };
 
-  // useEffect(() => {
-  //     loadShoes();
-  // }, []);
+  const handleHatChange = (event) => {
+    const value = event.target.value;
+    setHat(value);
+  };
 
-  // useEffect(() => {
-  //     loadBins();
-  // }, []);
+  const handleTopChange = (event) => {
+    const value = event.target.value;
+    setTop(value);
+  };
 
-  // }, [token]);
+  const handleBottomChange = (event) => {
+    const value = event.target.value;
+    setBottom(value);
+  };
 
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        height: "100vh",
-      }}
-    >
-      <form onSubmit={handleSubmit}>
-        <button
-          className="btn btn-primary"
-          type="submit"
-        >Submit Style</button>
-        <div className="form-group">
-          <label htmlFor="hat">Hats</label>
-          <select name="hat" value={hats} onChange={handleHatsChange}>
-            <option value="">Choose a Hat</option>
-            {hats.map((hat) => (
-              <option value={hat} key={hat.id}>
-                {hat.name}
-              </option>
-            ))}
-          </select>
-        </div>
-      </form>
+  const handleShoeChange = (event) => {
+    const value = event.target.value;
+    setShoe(value);
+  };
 
-      {/* <h3 style={{ color: "white" }}>Hats</h3>
-        <div style={boxStyle}>
-          {hats &&
-            hats.map((hat) => (
-              <div key={hat.id}>
-                <img
-                  src={hat.picture}
-                  style={{
-                    width: "100%",
-                    height: "100%",
-                    objectFit: "cover",
-                  }}
-                />
-              </div>
-            ))}
-        </div>
-        <h3 style={{ color: "white" }}>Tops</h3>
-        <div style={boxStyle}>
-          {tops &&
-            tops.map((top) => (
-              <div key={top.id}>
-                <img
-                  src={top.picture}
-                  style={{
-                    width: "100%",
-                    height: "100%",
-                    objectFit: "contain",
-                  }}
-                />
-              </div>
-            ))}
-        </div>
-        <h3 style={{ color: "white" }}>Bottoms</h3>
-        <div style={boxStyle}>
-          {bottoms &&
-            bottoms.map((bottom) => (
-              <div key={bottom.id}>
-                <img
-                  src={bottom.picture}
-                  style={{
-                    width: "100%",
-                    height: "100%",
-                    objectFit: "contain",
-                  }}
-                />
-              </div>
-            ))}
-        </div>
-        <h3 style={{ color: "white" }}>Shoes</h3>
-        <div style={boxStyle}>
-          {shoes &&
-            shoes.map((shoe) => (
-              <div key={shoe.id}>
-                <img
-                  src={shoe.picture}
-                  style={{
-                    width: "100%",
-                    height: "100%",
-                    objectFit: "contain",
-                  }}
-                />
-              </div>
-            ))}
-        </div>
-      </form> */}
-    </div>
-  );
-};
+  const boxStyle = {
+    width: "200px",
+    height: "200px",
+    backgroundColor: "#ccc",
+    margin: "10px",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "relative",
+    boxShadow: "0 3px 15px rgba(255, 255, 0, 0.5)",
+    borderRadius: "9px",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  };
 
-const boxStyle = {
-  width: "200px",
-  height: "200px",
-  backgroundColor: "#ccc",
-  margin: "10px",
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "center",
-  position: "relative",
-  boxShadow: "0 3px 15px rgba(255, 255, 0, 0.5)",
-  borderRadius: "9px",
-  backgroundSize: "cover",
-  backgroundPosition: "center",
+    const containerStyle = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  };
+
+return (
+  <div style={containerStyle}>
+    <h1 style={{ color: "white", textAlign: "center" }}>Hats</h1>
+    <form onSubmit={handleSubmit}>
+      <div style={{ ...boxStyle, textAlign: "center" }}>
+        <select name="hat" value={hat} onChange={handleHatChange}>
+          <option value="">Choose a Hat</option>
+          {hats.map((hat) => (
+            <option value={hat.picture} key={hat.id}>
+              {hat.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <h1 style={{ color: "white", textAlign: "center" }}>Tops</h1>
+      <div style={{ ...boxStyle, textAlign: "center" }}>
+        <select name="top" value={top} onChange={handleTopChange}>
+          <option value="">Choose a Top</option>
+          {tops.map((top) => (
+            <option value={top.picture} key={top.id}>
+              {top.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <h1 style={{ color: "white", textAlign: "center" }}>Bottoms</h1>
+      <div style={{ ...boxStyle, textAlign: "center" }}>
+        <select name="bottom" value={bottom} onChange={handleBottomChange}>
+          <option value="">Choose a Bottom</option>
+          {bottoms.map((bottom) => (
+            <option value={bottom.picture} key={bottom.id}>
+              {bottom.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <h1 style={{ color: "white", textAlign: "center" }}>Shoes</h1>
+      <div style={{ ...boxStyle, textAlign: "center" }}>
+        <select name="shoe" value={shoe} onChange={handleShoeChange}>
+          <option value="">Choose a Shoe</option>
+          {shoes.map((shoe) => (
+            <option value={shoe.picture} key={shoe.id}>
+              {shoe.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <button className="btn btn-primary" type="submit">
+        Submit Styles
+      </button>
+    </form>
+  </div>
+);
 };
 
 export default WardrobeForm;
