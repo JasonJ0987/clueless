4e28b3c4fef828b1738bf15af496062651886d94
Liland <phamliland@gmail.com>
2023-06-07 17:02:40 -0400

    Deleted comments

--- whatevr/main.py
+++ whatevr/main.py
@@ -1,11 +1,8 @@
 from fastapi import FastAPI
-
 from fastapi.middleware.cors import CORSMiddleware
 import os
-
 from api.utils.authenticator import authenticator
 from api.routers import accounts, weather, closet, tags
-
 
 app = FastAPI()
 
@@ -23,11 +20,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
 app.include_router(authenticator.router)
 app.include_router(accounts.router)
 app.include_router(weather.router)
 app.include_router(closet.router)
 app.include_router(tags.router)
-
-
